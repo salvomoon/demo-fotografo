028b3e4fa52a6c2ed12c415556ef53177dc33901
Salvatore Munaò <133042779+salvomoon@users.noreply.github.com>
2025-05-13 08:29:31 +0200

    Update vite.config.js
    fix deploy git pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src', // Aggiungi questa alias per facilitare l'importazione dei file
+    },
+  },
+  build: {
+    outDir: 'dist', // Specifica la cartella di output per la build
+  },
+  server: {
+    // Puoi anche aggiungere configurazioni per il server di sviluppo, se necessario
+    port: 3000,
+  },
 })
